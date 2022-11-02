06690e58e1e723b601f7cffd1eb0fd3a79c3f781
neptotech <85818947+neptotech@users.noreply.github.com>
2022-11-02 23:01:42 +0530

    Ai

--- LearningEngine.py
+++ LearningEngine.py
@@ -5,37 +5,110 @@ from math import inf
 import sys
 import codecs
 import json
+from tensorflow import summary
+import matplotlib.pyplot as plt
+from io import StringIO
+from datetime import datetime
+import tempfile
+import time
+
+PIECE_VALUES = {chess.PAWN: 1, chess.KNIGHT: 3, chess.BISHOP: 3,
+                chess.ROOK: 5, chess.QUEEN: 9, chess.KING: 0, }
+
+
+def material_count(new_board):
+    # count material in the new position for player to move
+
+    all_pieces = new_board.piece_map().values()
+
+    material_difference = 0
+
+    for piece in all_pieces:
+        value = PIECE_VALUES[piece.piece_type]
+        if piece.color == new_board.turn:
+            material_difference += value
+        else:
+            material_difference -= value
+
+    return material_difference
+
+
+def print_game(board):
+    new_board = chess.Board()
+    san_moves = []
+    for move in board.move_stack:
+        san_moves += [new_board.san(move)]
+        new_board.push(move)
+
+    to_print = []
+
+    for i in range(len(san_moves)):
+        if i % 2 == 0:
+            to_print.append("%d." % (i / 2 + 1))
+        to_print.append(san_moves[i])
+
+    print(" ".join(to_print))
+
+
+class CircleBuffer(list):
+    def __init__(self, max_size):
+        super().__init__()
+        self.cursor = 0
+        self.max_size = max_size
+
+    def add_item(self, x):
+        if len(self) < self.max_size:
+            self.append(x)
+        else:
+            # print("Buffer is full")
+            self[self.cursor] = x
+            self.cursor = (self.cursor + 1) % self.max_size
 
 
 class LearningEngine(MinimalEngine):
 
-    def __init__(self, *args, name=None, weights=None, weight_file="weights.json"):
+    def __init__(self, *args, name=None, weights=None, weight_file="weights.json",
+                 projection_seed=0, buffer_size=1000, batch_size=10, average_param=0.99):
         super().__init__(*args)
         self.name = name
 
+        self.random_state = np.random.RandomState(projection_seed)
+        self.projection = self.random_state.randn(395, 395)  # todo: make this more flexible
+        self.offset = 2 * np.pi * self.random_state.rand(395)
+
+        self.buffer = CircleBuffer(buffer_size)
+        self.batch_size = batch_size
+
+        self.grad_magnitude = 1
+        self.average_param = average_param
+        # weight_file = "weights.json"
+
         if weight_file:
+            print("Loading weights from file")
             # load weights from file
             with codecs.open(weight_file, 'r', encoding='utf-8') as fopen:
                 weight_test = fopen.read()
                 weight_list = json.loads(weight_test)
             self.weights = np.array(weight_list)
-
         elif weights is None:
             # initialize weights
+            print("Initializing new weights")
             starting_board = chess.Board()
             descriptor = self.features(starting_board)
             self.weights = np.random.rand(descriptor.size)
-
         else:
             self.weights = weights
 
-    @staticmethod
-    def features(board):
+    def features(self, board):
         """
         Returns a numerical vector describing the board position
         """
 
-        all_pieces = board.piece_map().values()
+        # mirror board if black's turn
+        if board.turn == chess.BLACK:
+            board = board.mirror()
+
+        all_pieces = board.piece_map().items()
 
         features = np.zeros(7)
 
@@ -48,130 +121,265 @@ class LearningEngine(MinimalEngine):
             chess.KING: 5
         }
 
-        for piece in all_pieces:
+        # add castling rights
+        cr = board.castling_rights
+
+        castling = [board.has_kingside_castling_rights(chess.WHITE),
+                    board.has_queenside_castling_rights(chess.WHITE),
+                    board.has_kingside_castling_rights(chess.BLACK),
+                    board.has_queenside_castling_rights(chess.BLACK)]
+
+        piece_grid = np.zeros((64, 6))
+
+        for position, piece in all_pieces:
             value = -1 if piece.color == board.turn else 1
 
-            features[index[piece.piece_type]] += value
+            type_index = index[piece.piece_type]
+
+            features[type_index] += value
+
+            piece_grid[position, type_index] = value
 
         if board.is_checkmate():
             features[6] = 1
 
-        return features
+        return self.random_project(np.concatenate((features, castling, piece_grid.ravel())))
+
+    def random_project(self, x):
+        projections = np.cos(self.projection.dot(x) + self.offset)
+        return np.concatenate((x, projections))
+
+    def action_score(self, board, move):
+        # calculate score
+        board.push(move)
+        descriptor = self.features(board)
+        board.pop()
+
+        score = self.weights.dot(descriptor)
+
+        return score
 
     def search(self, board, time_limit, ponder):
-
-        moves = list(board.legal_moves)
+        # returns a random choice among highest-scoring q values
+        moves = np.array(list(board.legal_moves))
 
         scores = np.zeros(len(moves))
 
         for i, move in enumerate(moves):
             # apply the current candidate move
-            board.push(move)
 
-            # calculate score
-            descriptor = self.features(board)
-            scores[i] = self.weights.dot(descriptor)
+            scores[i] = self.action_score(board, move)
 
-            board.pop()
+        best_moves = moves[scores == scores.max()]
 
-        probs = np.exp(scores)
-        probs /= np.sum(probs)
-
-        samples = np.random.multinomial(1, probs)
-        sampled_move = moves[np.min(np.argwhere(samples))]
-
-        return sampled_move
+        return np.random.choice(best_moves)
 
     def save_weights(self, filepath):
-        weights_list = self.weights.tolist()
+        weight_list = self.weights.tolist()
         with codecs.open(filepath, 'w', encoding='utf-8') as fopen:
-            json.dump(weights_list, fopen)  # ,separators=(',',':'),sort_keys=True, index = 4
+            json.dump(weight_list, fopen)
+
+    def q_learn(self, reward, prev_board, prev_move, new_board, learning_rate=0.001, discount=0.8):
+        # q(a, s) is estimate of discounted future reward after
+        #       making move a from s
+        # q(a, s) <- q(a, s) + learning_rate *
+        #                       (reward +  discount * max_{a'} q(a', s') - q(a, s))
+
+        # store position in buffer
+        prev_board.push(prev_move)
+        prev_features = self.features(prev_board)
+        prev_board.pop()
+        new_boards = []
+        for move in new_board.legal_moves:
+            new_board.push(move)
+            new_boards.append(self.features(new_board))
+            new_board.pop()
+        self.buffer.add_item((reward, prev_features, new_boards))
+
+        loss = 0
+        gradient = 0
+
+        for _ in range(self.batch_size):
+
+            i = self.random_state.randint(0, len(self.buffer))
+            batch_reward, batch_prev_features, batch_new_boards = self.buffer[i]
+
+            # compute q-learning lookahead score
+            if len(batch_new_boards) == 0:
+                max_future_score = 0
+            else:
+                max_future_score = max([self.weights.dot(features)
+                                        for features in batch_new_boards])
+
+            error = batch_reward + discount * max_future_score - self.weights.dot(batch_prev_features)
+
+            gradient -= error * batch_prev_features
+
+            loss += error ** 2
+
+        gradient /= self.batch_size
+
+        gradient = np.clip(gradient, -1, 1)
+
+        # uncomment this line to disable RMSProp
+        self.grad_magnitude = self.average_param * self.grad_magnitude + (1 - self.average_param) * gradient ** 2
+
+        # update weights
+        self.weights -= learning_rate * gradient / np.sqrt(self.grad_magnitude)
+
+        return loss / self.batch_size
 
 
 if __name__ == "__main__":
-    engine_white = LearningEngine(None, None, sys.stderr)
+
+    time_string = datetime.now().strftime("Y%Y M%m D%d-H%H M%M S%S")
+
+    # base_dir = tempfile.TemporaryDirectory().name
+    # base_dir = "/Users/bert/Desktop"
+    # # log_dir = '{}/logs/'.format(base_dir)
+    # print("Storing logs in {}".format(log_dir))
+    # writer = summary.create_file_writer(log_dir + time_string)
+    log_dir = 'logs/{}'.format(time_string)
+    writer = summary.create_file_writer(log_dir)
+
+    step = 0
+    start_time = time.time()
+
+    # Use this next line to load bot weights from disk
+    # engine_white = LearningEngine(None, None, sys.stderr)
+    # Use this next line to re-initialize bot
+    engine_white = LearningEngine(None, None, sys.stderr, weights=None, weight_file=None) #weights.json
+
     board = chess.Board()
 
-    engine_white.weights[0] = 1.0
+    engine_black = LearningEngine(None, None, sys.stderr, weights=None, weight_file=None)
 
-    engine_black = LearningEngine(None, None, sys.stderr)
-    # TODO: ch
-    # engine_black.weights = np.array([1., 3., 3., 5., 9., 0., 25.])
-    # engine_white.weights = np.array([1., 1., 1., 1., 1., 0., 25.])
+    # engine_black.weights[:7] = [1., 3., 3., 5., 9., 0., 100.]
+    # engine_black.weights[7:] = 0
+    # engine_white.weights[:7] = [1., 3., 3., 5., 9., 0., 100.]
+    # engine_white.weights[7:] = 0
+    engine_black.weights *= 0
+    engine_white.weights *= 0 #1
 
     wins = 0
     losses = 0
     draws = 0
 
-    max_moves = 100
+    max_moves = 60
+    eps_reset = 0
 
     while True:
+        # Occasionally update black to match white's weights
+        if step % 1000 == 0:
+            print("Updating black to match learned weights")
+            engine_black.weights = engine_white.weights.copy()
+            eps_reset = 0
+
+        queen_mate = '8/8/8/3Q4/8/3K4/8/4k3 w - - 2 2'  # mate in 2
+        # queen_mate = '8/8/8/2Q5/8/4K3/8/4k3 w - - 0 1'  # mate in 1
+        # smith_morra = 'rnbqkbnr/pp1ppppp/8/2p5/3PP3/8/PPP2PPP/RNBQKBNR b KQkq - 0 3'
         board = chess.Board()
 
         white_positions = []
-        black_positions = []
+        white_moves = []
+
+        # play a single game
 
         while not board.outcome() and board.fullmove_number < max_moves:
+            # play a game
             if board.turn == chess.WHITE:
-                black_positions.append(engine_white.features(board))
-                move = engine_white.search(board, 1000, True)
+                white_positions.append(board.copy(stack=False))
+
+                epsilon = 1 / np.sqrt((step - eps_reset) / 10 + 100)
+
+                # use epsilon-greedy strategy
+                if np.random.rand() < epsilon:
+                    move = np.random.choice(list(board.legal_moves))
+                else:
+                    move = engine_white.search(board, 1000, True)
+                white_moves.append(move)
             else:
-                white_positions.append(engine_black.features(board))
                 move = engine_black.search(board, 1000, True)
 
             # print(board.san(move))
             board.push(move)
 
-        outcome = board.outcome(claim_draw=True)
+        # do learning on all steps of the game
 
-        learning_rate = 0.0001 # TODO: ch
+        outcome = board.outcome()
 
+        rewards = []
+
+        q_losses = []
+
+        # do q-learning on each of white's moves
+        for i in range(len(white_moves) - 1):
+            reward = material_count(white_positions[i + 1]) - \
+                     material_count(white_positions[i])
+            rewards.append(reward)
+
+            q_loss = engine_white.q_learn(reward, white_positions[i], white_moves[i],
+                                          white_positions[i + 1])
+            q_losses.append(q_loss)
+
+        # final move
         if outcome and outcome.winner == chess.WHITE:
-            print("White wins")
-
-            # learn from white,wins
-            for position in white_positions:
-                engine_white.weights += learning_rate * position
-            # learn from black,losses
-            for position in black_positions:
-                engine_white.weights -= learning_rate * position
-
+            reward = 100
             wins += 1
-
         elif outcome and outcome.winner == chess.BLACK:
-            print("Black wins")
-
-            # learn from white,losses
-            for position in white_positions:
-                engine_white.weights -= learning_rate * position
-            # learn from black,wins
-            for position in black_positions:
-                engine_white.weights += learning_rate * position
-
+            reward = -100
             losses += 1
-
         else:
-            print("Draw")
+            reward = 0
+            draws += 1
 
-            # learn from white,draws
-            for position in white_positions:
-                engine_white.weights -= learning_rate * position
-            # learn from black,draws
-            for position in black_positions:
-                engine_white.weights -= learning_rate * position
+        rewards.append(reward)
 
-        draws += 1
+        episode_reward = np.sum(rewards)
 
-        # engine_white.weights[0] = 1
-        engine_white.weights = engine_white.weights.clip(min=0, max=50)# TODO: ch
+        q_loss = engine_white.q_learn(reward, white_positions[-1], white_moves[-1],
+                                      chess.Board('8/8/8/8/8/8/8/8 w - - 0 1'))
+        q_losses.append(q_loss)
 
+        # log diagnostic info
+        with writer.as_default():
+            summary.scalar('Reward', episode_reward, step)
+            summary.scalar('Result', reward / 100, step)
+            summary.scalar('Average Loss', np.mean(q_losses), step)
+            summary.scalar('P', engine_white.weights[0], step)
+            summary.scalar('N', engine_white.weights[1], step)
+            summary.scalar('B', engine_white.weights[2], step)
+            summary.scalar('R', engine_white.weights[3], step)
+            summary.scalar('Q', engine_white.weights[4], step)
+            summary.scalar('M', engine_white.weights[6], step)
+
+        if step % 10 == 0:
+            # plot weights
+            pieces = ['P', 'N', 'B', 'R', 'Q', 'K']
+            piece_weights = engine_white.weights[11:(11 + 64 * 6)].reshape((64, 6))
+            max_weight = piece_weights.max()
+
+            im_summaries = []
+
+            for i, p in enumerate(pieces):
+                slice = piece_weights[:, i].reshape((8, 8, 1)) / max_weight
+
+                with writer.as_default():
+                    summary.image("Weights-{}".format(p), [slice], step)
+
+        # print diagnostic info
         weights = engine_white.weights
-
-        print("P: {:.3f}, N: {:.3f}, B: {:.3f}, R: {:.3f}, Q: {:.3f}, K: {:.3f}, M: {:.3f}".format(
+        print("P: {:.2f}, N: {:.2f}, B: {:.2f}, R: {:.2f}, Q: {:.2f}, K: {:.2f}, M: {:.2f}".format(
             weights[0], weights[1], weights[2], weights[3], weights[4], weights[5],
             weights[6]))
 
-        print("Wins: {}. Losses: {}. Draws {}, win rate{:.2f}, w/l:{:.2f}".format
-              (wins, losses, draws, wins / (wins + losses + draws), wins / (losses + 1e-16)))
+        print("Wins: {}. Losses: {}. Draws: {}. Win rate: {:.2f}, W/L: {:.2f}".format(
+            wins, losses, draws, wins / (wins + losses + draws), wins / (losses + 1e-16)))
 
         engine_white.save_weights("weights.json")
+
+        step += 1
+
+        elapsed_time = time.time() - start_time
+
+        print("Played {} games ({:.2f} games/sec)".format(step, step / elapsed_time))
--- test_engine.py
+++ test_engine.py
@@ -4,7 +4,8 @@ from custom_engine import ScoreEngine, material_count, tiebreakers
 import sys
 import math
 import time
-from LearningEngine import LearningEngine
+from LearningEngine import LearningEngine, CircleBuffer
+from LearningEngine import material_count as material_count_learner
 import numpy as np
 
 missing_rook_white = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBN1 w Qkq - 0 1'
@@ -16,9 +17,15 @@ bad_check = '8/8/Q7/1P5k/8/8/6q1/1K6 w - - 0 1'
 opening_error = 'r1bqkb1r/pppn1ppp/3Pp3/4n2Q/8/3B4/PPPP1PPP/RNB1K1NR w KQkq - 1 7'
 opening_error_flipped = 'r1bqkb1r/pppn1ppp/3Pp3/4n2Q/8/3B4/PPPP1PPP/RNB1K1NR b KQkq - 1 7'
 
-imbalance_material_w = 'r1b1k1nr/pppppppp/8/8/8/8/PPP1PPPP/1NBQKBN1 w kq - 0 1'
-imbalance_material_b = 'r1b1k1nr/pppppppp/8/8/8/8/PPP1PPPP/1NBQKBN1 b kq - 0 1'
+imbalanced_material_white = 'r1b1k1nr/pppppppp/8/8/8/8/PPP1PPPP/1NBQKBN1 w kq - 0 1'
+imbalanced_material_black = 'r1b1k1nr/pppppppp/8/8/8/8/PPP1PPPP/1NBQKBN1 b kq - 0 1'
 
+modified_stafford = 'r1bqkb1r/pppp1ppp/2n2n2/4N3/4P3/8/PPPP1PPP/RN1QKB1R w KQkq - 0 1'
+reversed_mod_stafford = 'rn1qkb1r/pppp1ppp/8/4p3/4n3/2N2N2/PPPP1PPP/R1BQKB1R b KQkq - 0 1'
+
+giuoco_piano = 'r1bqk1nr/pppp1ppp/2n5/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4'
+
+queen_mate = '8/8/5Q2/2k5/4K3/8/8/8 w - - 0 1'
 
 class EngineTestCase(unittest.TestCase):
 
@@ -135,32 +142,165 @@ class EngineTestCase(unittest.TestCase):
 
         print("Tiebreaker score is {}".format(score))
 
-    def test_learning_feeatures(self):
+    def test_learning_features(self):
+        board = chess.Board(imbalanced_material_white)
+
         engine = LearningEngine(None, None, sys.stderr)
 
-        board = chess.Board(imbalance_material_w)
         features = engine.features(board)
 
-        flipped_board = chess.Board(imbalance_material_b)
+        flipped_board = chess.Board(imbalanced_material_black)
+
         flipped_features = engine.features(flipped_board)
 
-        print("fe white")
+        print("Features for white to move:")
         print(features)
-        print("fe white")
+        print("Features for black to move:")
         print(flipped_features)
 
         self.assertTrue(np.allclose(features, -flipped_features))
 
     def test_weight_saving(self):
         engine = LearningEngine(None, None, sys.stderr)
-        print("initialised with weights /n\n",engine.weights)
+        print("Initialized engine with weights")
+        print(engine.weights)
 
-        path = "weights.json"
+        path = "weights.json"  # todo: make this a proper temp file
         engine.save_weights(path)
+
         new_engine = LearningEngine(None, None, sys.stderr, weight_file=path)
+        print("New engine loaded with weights")
         print(new_engine.weights)
 
         self.assertTrue(np.allclose(new_engine.weights, engine.weights))
+
+    def test_random_feature_seeding(self):
+        engine1 = LearningEngine(None, None, sys.stderr)
+        engine2 = LearningEngine(None, None, sys.stderr)
+
+        self.assertTrue(np.allclose(engine1.projection, engine2.projection))
+        self.assertTrue(np.allclose(engine1.offset, engine2.offset))
+
+    def test_circ_buff(self):
+        buffer = CircleBuffer(max_size=4)
+
+        buffer.add_item('a')
+        buffer.add_item('b')
+        buffer.add_item('c')
+        buffer.add_item('d')
+
+        print(buffer)
+
+        self.assertEqual(tuple(buffer), ('a', 'b', 'c', 'd'))
+
+        buffer.add_item('e')
+        buffer.add_item('f')
+
+        print(buffer)
+        self.assertEqual(tuple(buffer), ('e', 'f', 'c', 'd'))
+
+    def test_board_mirroring(self):
+        engine = LearningEngine(None, None, sys.stderr, weight_file=None)
+
+        white_board = chess.Board(modified_stafford)
+        white_features = engine.features(white_board)
+
+        black_board = chess.Board(reversed_mod_stafford)
+        black_features = engine.features(black_board)
+
+        self.assertTrue(np.allclose(white_features, black_features))
+
+    def test_buffering(self):
+        engine = LearningEngine(None, None, sys.stderr, buffer_size=10, weight_file=None)
+
+        self.assertEquals(len(engine.buffer), 0)
+
+        board = chess.Board(opening_error)
+
+        # make move for white
+        moves = list(board.legal_moves)
+        move = moves.pop()
+
+        new_board = board.copy()
+        new_board.push(move)
+
+        # make move for black
+        moves = list(new_board.legal_moves)
+        black_move = moves.pop()
+
+        new_board.push(black_move)
+
+        engine.q_learn(0, board, move, new_board)
+
+        self.assertEquals(len(engine.buffer), 1)
+
+    def test_q_loss_reduction(self):
+        starting_board = chess.Board(giuoco_piano)
+
+        bad_line = ['Nxe5', 'Nxe5']
+        good_line = ['c3', 'Nf6']
+
+        bad_board = starting_board.copy()
+        bad_move = starting_board.parse_san(bad_line[0])
+
+        for san in bad_line:
+            bad_board.push(bad_board.parse_san(san))
+
+        bad_reward = material_count_learner(bad_board) - material_count(starting_board)
+
+        good_board = starting_board.copy()
+        good_move = starting_board.parse_san(good_line[0])
+
+        for san in good_line:
+            good_board.push(good_board.parse_san(san))
+
+        good_reward = material_count_learner(good_board) - material_count(starting_board)
+
+        print("Line {} leads to reward {}".format(bad_line, bad_reward))
+        print("Line {} leads to reward {}".format(good_line, good_reward))
+
+        engine = LearningEngine(None, None, sys.stderr, weights=None, weight_file=None)
+        engine.weights *= 0
+
+        init_bad_estimate = engine.action_score(starting_board, bad_move)
+        init_good_estimate = engine.action_score(starting_board, good_move)
+
+        print("Initial engine estimated reward of {} is {}".format(
+            bad_line[0], init_bad_estimate))
+        print("Initial engine estimated reward of {} is {}".format(
+            good_line[0], init_good_estimate))
+
+        losses = []
+
+        for _ in range(100):
+            bad_loss = engine.q_learn(bad_reward, starting_board, bad_move, bad_board)
+            good_loss = engine.q_learn(good_reward, starting_board, good_move, good_board)
+
+            losses.append(bad_loss + good_loss)
+
+        final_bad_estimate = engine.action_score(starting_board, bad_move)
+        final_good_estimate = engine.action_score(starting_board, good_move)
+
+        print("Final engine estimated reward of {} is {}".format(
+            bad_line[0], final_bad_estimate))
+        print("Final engine estimated reward of {} is {}".format(
+            good_line[0], final_good_estimate))
+
+        print("Losses: {}".format(losses))
+
+        self.assertLess(losses[-1], losses[0])
+        self.assertLess(losses[-1], 0.1)
+
+        init_error = np.abs(init_good_estimate - good_reward) + np.abs(init_bad_estimate - bad_reward)
+        final_error = np.abs(final_good_estimate - good_reward) + np.abs(final_bad_estimate - bad_reward)
+
+        self.assertLess(final_error, init_error)
+
+        # This only seems to work if we initialize to all zeros. Maybe because we don't have any base cases?
+
+    def test_mate_learn(self):
+        board = LearningEngine()
+
 
 if __name__ == '__main__':
     unittest.main()
